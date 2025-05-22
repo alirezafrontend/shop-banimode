23326be7bf58b9a6fc5b3dcc6af287b31a2c642b
Alireza hasanpour <alirezahasanpourweb19921371@gmail.com>
2025-05-23 01:01:41 +0330

    Update BannerShop.jsx

--- src/component/shop/BannerShop.jsx
+++ src/component/shop/BannerShop.jsx
@@ -6,7 +6,7 @@ const BannerShop = () => {
     <div className="bg-[#f16422] bg-[url('/shop/bg-timer.png')] bg-cover bg-center bg-no-repeat h-[100px] flex items-center justify-start rounded-[10px] px-[20px]">
       <div>
         <div className="flex items-center gap-[15px]">
-          <Image src="/shop/fire.svg" width={45} height={45} alt="fire" />
+          <Image src="/shop/fire.svg" width={45} height={45} alt="fire" unoptimized/>
           <h1 className="text-[#fff] text-[22px] md:text-[25px] font-semibold">
             شگفت‌انگـــــیز‌های روز
           </h1>
