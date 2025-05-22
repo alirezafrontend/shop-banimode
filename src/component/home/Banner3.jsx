661feb2e27f92aba90ce5a91705811a20bb06408
Alireza hasanpour <alirezahasanpourweb19921371@gmail.com>
2025-05-23 00:52:45 +0330

    Update Banner3.jsx

--- src/component/home/Banner3.jsx
+++ src/component/home/Banner3.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Banner3 = () => {
   return (
     <div className="min-h-[250px] md:min-h-[350px] overflow-hidden rounded-[20px]">
-      <Image className="rounded-[20px] h-full object-fit-cover" src="/banner/3-1.webp" width={1200} height={437} alt="banner2"/>
+      <Image className="rounded-[20px] h-full object-fit-cover" src="/banner/3-1.webp" width={1200} height={437} alt="banner2" unoptimized/>
     </div>
   );
 };
